960a5c86063b1ece78f7e62b31a32ae5e7f311d3
ferreolpennel <ferreol@pennel.org>
2019-02-13 20:51:23 +0100

    Update connect_to_uav

--- connect_to_uav.py
+++ connect_to_uav.py
@@ -4,37 +4,65 @@
 import os
 import csv
 import re
+import netifaces
+import time
 
-#création d'objet correspondant au drones
+#Class object for the UAV
 class AP_drone:
-    """Classe définissant un Wifi de drone caractérisée par:
-        - BSSID @Mac du drone
-        - ESSID Nom du réseau Wifi"""
+    """Class which defines AP of the UAV, caracterized by:
+        - BSSID Mac address of the UAV
+        - ESSID Name of the Wifi"""
     def __init__(self, bssid, essid):
         self.bssid = bssid
         self.essid = essid
+
+#Class object for the clients of the UAV
+class Client:
+    """ Class which defines clinets connected to the UAV and caracterized by:
+        - Mac address
+        - IP address """
+    def __init__(self, mac, ip):
+        self.mac = mac
+        self.ip = ip
 
 
 #mac addres of parrot drones: A0:14:3D 90:3A:E6 90:03:B7 00:26:7E 00:12:1C. Regex
 drone_macs = [ r"^90:03:B7", r"^A0:14:3D", r"^90:3A:E6", r"^00:26:7E", r"^00:12:1C"]
 drone_list = []
-#interfaces of wifi card
-interface = "wlo1"
-interface_mon = interface+"mon"
+
+client_list = []
+
+# iface = ""
+# iface_mon = ""
+
+#ifaces of wifi card
+
+iface_list = netifaces.interfaces()
+n = len(iface_list)
+c = 0;
+msg = ""
+while n>c:
+    msg += "{0} : {1} \n".format(iface_list[c], c)
+    c+=1
+msg+= "Your choice ? :"
+
+id_iface = input("Choose your iface:\n"+msg)
+iface = iface_list[int(id_iface)]
+iface_mon = iface+"mon"
 
 def start_airmon():
     #switch wifi to monitor mode to detect the wifi's drone
-    cmd = "sudo ifconfig {0} down".format(interface)
+    cmd = "sudo ifconfig {0} down >>/dev/null 2>>/dev/null".format(iface)
     os.system(cmd)
 
-    os.system("sudo airmon-ng check kill")
-    cmd = "sudo airmon-ng start {0}".format(interface)
+    os.system("sudo airmon-ng check kill >>/dev/null 2>>/dev/null")
+    cmd = "sudo airmon-ng start {0} >>/dev/null 2>>/dev/null".format(iface)
     os.system(cmd)
 
 def stop_airmon():
-    cmd = "sudo airmon-ng stop {0}".format(interface_mon)
+    cmd = "sudo airmon-ng stop {0} >>/dev/null 2>>/dev/null".format(iface_mon)
     os.system(cmd)
-    cmd = "sudo ifconfig {0} up".format(interface)
+    cmd = "sudo ifconfig {0} up >>/dev/null 2>>/dev/null".format(iface)
     os.system(cmd)
     os.system("sudo service NetworkManager start")
 
@@ -43,7 +71,7 @@ def scan_wifi():
     #file of AP
     drone_list = "drone_list"
     try:
-        cmd = "sudo airodump-ng -t OPN --output-format csv -w {0} {1} >>/dev/null 2>>/dev/null".format(drone_list, interface_mon)
+        cmd = "sudo airodump-ng -t OPN --output-format csv -w {0} {1} >>/dev/null 2>>/dev/null".format(drone_list, iface_mon)
         os.system("gnome-terminal -- {0}".format(cmd))
         os.system("sleep 15")
     except Exception as e:
@@ -58,16 +86,22 @@ def find_drone():
             if n == 15:
                 if re.match(drone_macs[0],row[0]) or re.match(drone_macs[1],row[0]) or re.match(drone_macs[2],row[0]) or re.match(drone_macs[3],row[0]) or re.match(drone_macs[4],row[0]) :
                     drone_list.append(AP_drone(row[0], row[13]))
-        print(drone_list[0].essid)
-    cmd = "sudo rm drone_list-01.csv"
+        print("Target : {0}".format(drone_list[0].essid))
+    # cmd = "sudo rm drone_list-0*"
+    # os.system(cmd)
+
+def connect(drone):
+    cmd = "sudo iwconfig {0} essid {1} && sudo dhclient".format(iface, drone.essid)
     os.system(cmd)
 
 
 def main():
+    #init_iface()
     start_airmon()
     scan_wifi()
-    stop_airmon()
     find_drone()
+    stop_airmon()
+    connect(drone_list[0])
 
 #if __name__ == 'main':
 main()
